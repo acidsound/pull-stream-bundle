206066f8148de1a7a114b5b5c502e80c2ac00ca4
JaehoLee <spectrick@gmail.com>
2019-03-24 02:45:22 +0900

    fix: remove extra semicolons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const pull = require('pull-stream');
-const Pushable = require('pull-pushable');
-const Notify = require('pull-notify');
-const Stringify = require('pull-stringify');
+const pull = require('pull-stream')
+const Pushable = require('pull-pushable')
+const Notify = require('pull-notify')
+const Stringify = require('pull-stringify')
 
 module.exports = {
   pull,
   Stringify,
   Pushable,
   Notify
-};
+}
